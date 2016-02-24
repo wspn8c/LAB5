5b908f500523e3250c310bd6ef1ecbd79cf72a16
William Scott Peery <wspn8c@mail.umkc.edu>
2016-02-24 10:38:12 -0600

    Added files via upload

--- Lab5.cpp
+++ Lab5.cpp
@@ -1,6 +1,6 @@
-// Your name
-// Today's date
-// Lab #4
+// Scott Peery
+// 2/22/2016
+// Lab #5
 
 #include "LibraryBook.h"
 #include <fstream>
@@ -13,6 +13,12 @@ int main() {
     ifstream finBooks("books.txt");
 	ifstream finISBN("isbns.txt");
 	ofstream fout("checkedout.txt");
+	string TempTitle, TempAuthor, TempISBN;
+	LibraryBook BooksArray[15];
+	int index = 0;
+	LibraryBook checker;
+	int ISBNarray[100000000];
+	
 
 	// Check for file errors first
 	if(!finBooks || !finISBN)
@@ -22,10 +28,30 @@ int main() {
 		return 1;
 	}
 
+	while (!finBooks.eof())
+	{
+		getline(finBooks,TempTitle); //read line 1
+		getline(finBooks, TempAuthor);//read line 2
+		getline(finBooks, TempISBN);// read line 3
+		LibraryBook Book(TempTitle, TempAuthor, TempISBN); // put first 3 lines in book
+		BooksArray[index] = Book;//assign book to array
+		//cout << BooksArray[index].getTitle()<<"\t"<< BooksArray[index].getAuthor()<<"\t"<< BooksArray[index].getISBN() << endl;//test input value
+		index++;
+	}
+	//cout << BooksArray[9].getTitle() << "\t" << BooksArray[9].getAuthor() << "\t" << BooksArray[9].getISBN()<<"\t"<<BooksArray[9].isCheckedOut() << endl; //testing output
+	//system("pause");
 
-    // Write program in here
+	int count = 0;
+	while (!finISBN.eof())//read in ISBNs
+	{
+		string number;
+		finISBN >> number; //read in first ISBN
+		
 
-
+	}
+	//fout << "Books checked out:\n";
+	//fout << "------------------\n";
+	//fout << "Title\t" << "Author\t" << "ISBN\n";
 
 
 	// Close the files at the end
